08a6b2a5ee9ca2b038f55326097129cbcba358f7
MartinDeLaCruz.69 <martindelacruz169.com>
2022-07-04 17:58:25 -0500

    Feat: The required code is added to the Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,1 +1,27 @@
- 
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css"
+import  Main  from "./Main.js"
+import Infos from "./Info.js"
+import Me from "./About.js"
+import Dir from "./Navbar.js"
+
+const Home = () => {
+  const Inicio = () => <h1>Home</h1>;
+  const Pokemon = () => <h1>Pokemon Info</h1>;
+  const About = () => <h1>190170</h1>;
+  return (
+    <>
+    <Dir />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/pokemons" element={<Main />}></Route>
+          <Route path="/pokemonsinfo" element={<Infos  />}></Route>
+          <Route path="/matricula" element={<Me />}></Route>
+        </Routes>
+      </BrowserRouter>
+
+    </>
+  );
+};
+export default Home;
