2219b71bc19387868914d5173cc1c147d2105c7a
MartinDeLaCruz.69 <martindelacruz169.com>
2022-07-04 18:17:56 -0500

    Fixes an error in the part of the .js

--- src/Home.js
+++ src/Home.js
@@ -2,12 +2,11 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css"
 import  Main  from "./Main.js"
-import Infos from "./Info.js"
 import Me from "./About.js"
 import Dir from "./Navbar.js"
 
-const Home = () => {
-  const Inicio = () => <h1>Home</h1>;
+const Inicio = () => {
+  const Home = () => <h1>Home</h1>;
   const Pokemon = () => <h1>Pokemon Info</h1>;
   const About = () => <h1>190170</h1>;
   return (
@@ -16,7 +15,7 @@ const Home = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/pokemons" element={<Main />}></Route>
-          <Route path="/pokemonsinfo" element={<Infos  />}></Route>
+          <Route path="/pokemonsinfo" element={<Info  />}></Route>
           <Route path="/matricula" element={<Me />}></Route>
         </Routes>
       </BrowserRouter>
@@ -24,4 +23,4 @@ const Home = () => {
     </>
   );
 };
-export default Home;
+export default Inicio;
