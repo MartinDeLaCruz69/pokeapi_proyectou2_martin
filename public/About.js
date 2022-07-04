9c5ffd2c325b35c60f205e7f2c0f5a931dc8f5b1
MartinDeLaCruz.69 <martindelacruz169.com>
2022-07-04 18:05:32 -0500

    Feat: The code is added for the About.js that you have to need

---
+++ public/About.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./App.css"
+
+const Me = () => {
+
+    return (
+        <div>
+            <div>
+                <img className="Images" src="./img/me.jpg" alt="perfil"></img>
+            </div>
+            <div>
+            <h1>Juan Martin Esparza de la Cruz</h1><br></br>
+            <h5>Universidad Tecnológica de Aguascalientes</h5><br></br>
+
+            <div class="hobbies">
+        <table>
+            <tr>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>2</td>
+            </tr>
+            <tr>
+                <td>3</td>
+            </tr>
+            <tr>
+                <td>4</td>
+            </tr>
+            <tr>
+                <td>5</td>
+            </tr>
+        </table>
+    </div>
+            </div>
+        </div>
+     );
+
+};
+export default Me;
