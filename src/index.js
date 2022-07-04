7704ea7a31a7f2d427a01bab04cdf0320c6dc0ab
MartinDeLaCruz.69 <martindelacruz169.com>
2022-07-04 18:40:13 -0500

    Ci: The Navbar is modified.js in the code

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Home';
-import React from "react";
-import { NavbarWrapper } from "./Styles/NavbarStyles";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,16 +10,5 @@ root.render(
     <App /> 
   </React.StrictMode>
 );
-function Navbar({ open }) {
-  return (
-    <NavbarWrapper open={open}>
-      <a href="#">Link</a>
-      <a href="#">Link</a>
-      <a href="#">Link</a>
-      <a href="#">Link</a>
-    </NavbarWrapper>
-  );
-}
 
-export default Navbar;
 
