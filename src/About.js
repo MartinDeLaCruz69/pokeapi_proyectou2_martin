728a6fd5f9c64fa2282adb404f2d81b742f08df7
MartinDeLaCruz.69 <martindelacruz169.com>
2022-07-04 21:25:08 -0500

    Feat: Add new file to Abouts

--- src/About.js
+++ src/About.js
@@ -15,19 +15,34 @@ const Begin = () => {
             <div class="hobbies">
         <table>
             <tr>
-                <td>1</td>
+                <td>1.- Me gusta practicar boxeo</td>
+                <div>
+                <img className="img" src="./img/boxeo.jpg" alt="box"></img>
+            </div>
             </tr>
             <tr>
-                <td>2</td>
+                <td>2.- Disfruto comer discada.</td>
+                <div>
+                <img className="img" src="./img/discada.jpg "alt=""></img>
+            </div>
             </tr>
             <tr>
-                <td>3</td>
+                <td>3.- Jugar videojuegos. </td>
+                <div>
+                <img className="img" src="./img/games.jpg "alt=""></img>
+            </div>
             </tr>
             <tr>
-                <td>4</td>
+                <td>4.- Me gusta escuchar todo tipo de música. </td>
+                <div>
+                <img className="img" src="./img/Music.png "alt=""></img>
+            </div>
             </tr>
             <tr>
-                <td>5</td>
+                <td>5.- Me gusta viajar</td>
+                <div>
+                <img className="img" src="./img/viajar.jpeg "alt=""></img>
+            </div>
             </tr>
         </table>
     </div>
